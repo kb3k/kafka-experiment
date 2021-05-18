61c6a5f7b54ce2e64af89591254c2927f2ae0225
Kerry Kurcz <kurczkerry@gmail.com>
2021-05-17 21:42:15 -0500

    hitting a wall in the insturx heer

--- kaf/producer_raw_recipes.py
+++ kaf/producer_raw_recipes.py
@@ -44,7 +44,9 @@ def get_recipes(headers):
 def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
+        print("key_bytes : ", key_bytes) 
         value_bytes = bytes(value, encoding='utf-8')
+        print("value_bytes : ", value_bytes)
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
         producer_instance.flush()
         print('Message published successfully.')
